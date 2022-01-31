3edef5ee09c63bfaf3a14ac2aa4d3810d19e54e3
GyeongCheol07 <skrudcjf1@naver.com>
2022-01-31 15:13:40 +0900

    Final Calender Project

--- src/eita/calender/Calender.java
+++ src/eita/calender/Calender.java
@@ -1,28 +1,86 @@
 package eita.calender;
 
-import java.util.Scanner;
-
 public class Calender {
 	
+	//평년,윤년 배열
 	private static int[] Max_Days = {31,28,31,30,31,30,31,31,30,31,30,31};
+	private static int[] Leap_Max_Days = {31,29,31,30,31,30,31,31,30,31,30,31};
 	
-	//입력한 달의 해당하는 Max_Days배열값을 리턴해주는 함수
-	public int getMaxDaysOfMonth(int month) {
-		return Max_Days[month-1];
+	//윤년/평년 판단 함수
+	public boolean isLeapYear(int year) {
+		if(year%4 == 0 && (year%100 != 0 || year%400 == 0)) {
+			return true;
+		}			
+		return false;
 	}
+	
+	//입력한 연도의 해당하는 Max_Days 배열값을 리턴해주는 함수
+	public int getMaxDaysOfMonth(int year, int month) {
+		if(isLeapYear(year)) {
+			return Leap_Max_Days[month-1];
+		} else {
+			return Max_Days[month-1];
+		}
+	}
+	
+	//시작하는 요일을 받아 숫자로 리턴해주는 함수
+	public int startDay(String day) {
+		if(day.equals("SUN")) return 7;
+		else if(day.equals("MON")) return 1;
+		else if(day.equals("TUE")) return 2;
+		else if(day.equals("WED")) return 3;
+		else if(day.equals("THU")) return 4;
+		else if(day.equals("FRI")) return 5;
+		else if(day.equals("SAT")) return 6;
+		else {
+			System.out.println("잘못입력하셨습니다.");
+			return 0;
+		}
+	}
+	
+	//자동으로 첫번째 요일 계산 함수(1970년1월1일 목요일기준)
+	public int getWeekDay(int year, int month) {
+		int syear = 1970;
+		int standardWeekday = 4;
+		
+		int count = 0;
+		
+		//연도 체크
+		for(int i=syear ; i<year ; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
+		}
+		//월 체크
+		for(int i=1 ; i<=(month-1) ; i++) {
+			int delta = getMaxDaysOfMonth(year, i);
+			count += delta;
+		}
+		
+		//요일 추출
+		int weekday = (count + standardWeekday) % 7;
+		return weekday;
+	}
+	
 	public void printCalender(int year, int month) {
+		//고정틀
 		System.out.printf("      <<%4d년 %3d월>>   \n", year, month);
 		System.out.println("SUN MON TUE WED THU FRI SAT");
 		System.out.println("----------------------------");
-		int maxDay = getMaxDaysOfMonth(month);
+		
+		int weekday = getWeekDay(year,month);
+		//공백입력
+		if(weekday<7) {
+			for(int i=0 ; i<weekday ; i++) {
+				System.out.print("    ");
+			}
+		}
+		//날짜입력
+		int maxDay = getMaxDaysOfMonth(year, month);
 		for(int i=1 ; i<= maxDay ; i++) {
 			System.out.printf("%4d",i);
-			if(i % 7 == 0) {
+			if(i % 7 == (7-weekday)) {
 				System.out.println();
 			}
 		}
-		
 	}
-	
-	
 }
--- src/eita/calender/Prompt.java
+++ src/eita/calender/Prompt.java
@@ -4,37 +4,29 @@ import java.util.Scanner;
 
 public class Prompt {
 	
-	private static String PROMPT = "cal> ";
-	
 	public void runPrompt() {
-		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		
+
+		//연도,월을 입력받아 달력을 출력하는 프로그램
 		Scanner scan = new Scanner(System.in);
 		Calender cal = new Calender();
 				
-		//for문을 이용한 반복 진행(횟수가 정해진 반복)
-//		System.out.print("반복 횟수를 입력하세요 : ");
-//		int repeat = scan.nextInt();
-//		
-//		for(int i=0 ; i<repeat ; i++) {
-//			System.out.println("월을 입력하세요 : ");
-//			System.out.print(prom.PROMPT);
-//			int month = scan.nextInt();
-//			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
-//		}
-				
 		//while문을 이용한 반복 진행(횟수 정하지 않은 반복)
 		while(true) {
-			System.out.println("월을 입력하세요(-1입력시 종료)");
-			System.out.print(PROMPT);
-			int month = scan.nextInt();
-			if(month == -1) {
+			System.out.println("연도를 입력하세요.(-1 입력시 종료)");
+			System.out.print("YEAR>");
+			int year = scan.nextInt();
+			if(year == -1) {
 				break;
-			}else if(month > 12) {
+			}
+			System.out.println("월을 입력하세요.");
+			System.out.print("MONTH>");
+			int month = scan.nextInt();
+			
+			if(month > 12) {
 				System.out.println("다시 입력해주세요.");
 				continue;
 			}else {
-				cal.printCalender(2021, month);
+				cal.printCalender(year, month);
 				System.out.println();
 			}
 		}
